12a1cd6f8fe024559fc9c8a1f11952ba52e1d70e
DevDarji16 <devdarji033@gmail.com>
2025-06-28 12:37:34 +0530

    ping added for cron job

--- Talentlink/urls.py
+++ Talentlink/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from .views import ping
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
+    path('ping/', ping),
 ]
