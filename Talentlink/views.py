de92dad465cbe10f9fc3493bba29563fecf305cc
DevDarji16 <devdarji033@gmail.com>
2025-09-08 10:24:25 +0530

    added get_token method

--- Talentlink/views.py
+++ Talentlink/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.middleware.csrf import get_token
 
 def ping(request):
     return JsonResponse({'status':'ok'})
